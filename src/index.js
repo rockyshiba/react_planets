c8971bcdb316a8d52739021ef7c62fea49067e5b
rockyshiba <leejsitu@gmail.com>
2018-10-07 23:04:34 -0400

    Click events initiated

--- src/index.js
+++ src/index.js
@@ -10,11 +10,15 @@ class Planets extends React.Component{
         this.state = {
 
             // setting up a property to hold an array of planets
-            planets: []
+            planets: [],
+
+            // property to hold a single planet
+            planet: null
         }
 
         // because of odd nature of javascript, you need to bind methods to the component if you want the method to be invoked on events 
         this.getPlanets = this.getPlanets.bind(this);
+        this.getPlanet = this.getPlanet.bind(this);
     }
 
     // method to retrieve list of planets
@@ -28,6 +32,14 @@ class Planets extends React.Component{
             );
     }
 
+    getPlanet(planetId){
+        axios.get('http://ghosteacher.com/apis/planets.php?apikey=pineapple&p_id=' + planetId).then(
+            response => this.setState({
+                planet: response.data
+            })
+        );
+    }
+
     // this is a predefined method that fires when the component loads on page
     componentDidMount(){
         // when the component loads, populate the planets array using the getPlanets method
@@ -36,12 +48,23 @@ class Planets extends React.Component{
 
     render(){
         return(
-            <ul>
-                {
-                    // use .map to iterate through an array
-                    this.state.planets.map((planet) => <li key={planet.id}>{planet.name}</li>)
+            <div id="planetInfo">
+                <ul>
+                    {
+                        // use .map to iterate through an array
+                        // (e) => so it doesn't loop or call right away
+                        this.state.planets.map((planet) => <li key={planet.id} onClick={(e) => this.getPlanet(planet.id)}>{planet.name}</li>)
+                    }
+                </ul>
+                {this.state.planet &&
+                    <div>
+                        <h2>{this.state.planet.name}</h2>
+                        <p>
+                            {this.state.planet.description}
+                        </p>
+                    </div>
                 }
-            </ul>
+            </div>
         );
     }
 }
