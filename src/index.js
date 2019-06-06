c747f401dd0b232a0983de8b04edb5a373b707de
LeeS <leejsitu@gmail.com>
2019-06-06 23:33:18 +0900

    npm package updates

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 
+
 class Planets extends React.Component{
     constructor(props){
         super(props);
@@ -10,11 +11,13 @@ class Planets extends React.Component{
         this.state = {
 
             // setting up a property to hold an array of planets
-            planets: []
+            planets: [],
+            planet: null
         }
 
         // because of odd nature of javascript, you need to bind methods to the component if you want the method to be invoked on events 
         this.getPlanets = this.getPlanets.bind(this);
+        this.getPlanet = this.getPlanet.bind(this);
     }
 
     // method to retrieve list of planets
@@ -28,6 +31,16 @@ class Planets extends React.Component{
             );
     }
 
+    // method to retrieve a single planet
+    getPlanet(idOfPlanet){
+        axios.get(`http://ghosteacher.com/apis/planets.php?apikey=pineapple&p_id=${idOfPlanet}`)
+        .then(
+            response => this.setState({
+                planet: response.data
+            })
+        );
+    }
+
     // this is a predefined method that fires when the component loads on page
     componentDidMount(){
         // when the component loads, populate the planets array using the getPlanets method
@@ -39,9 +52,29 @@ class Planets extends React.Component{
             <ul>
                 {
                     // use .map to iterate through an array
-                    this.state.planets.map((planet) => <li key={planet.id}>{planet.name}</li>)
+                    this.state.planets.map(
+                        (planet) => <li 
+                        key={planet.id} 
+                        onClick={this.getPlanet(planet.id)}
+                        >
+                        {planet.name}
+                        </li>)
                 }
             </ul>
+        );
+    }
+}
+
+class Planet extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <div className="planet">
+                <h2>{this.props.planet.name}</h2>
+            </div>
         );
     }
 }
